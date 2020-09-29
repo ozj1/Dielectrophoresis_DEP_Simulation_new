b99f5fc3db1c6d119c8f9a77ca29e5f33352ebf3
ozj1 <44460202+ozj1@users.noreply.github.com>
2020-09-29 11:09:42 -0400

    a prticle class carrying all the values and properties of a prticle was added, all the code was modified accordingly

--- Project3/Source.cpp
+++ Project3/Source.cpp
@@ -22,7 +22,7 @@ using namespace std;
 #define pi 3.141592653589793
 double eps0 = 8.854187817e-12, kT = 4.0453001636e-21, em = 78.5 * eps0, ep = 3.8 * eps0, Di = 1;//Di is in um unit here
 double Vpp = 40, dg = 8e-6; //for N,m,j,kg,s units, Ro density kg/m3, for N, m, j, kg, s, C units, eps0= 8.85e-12 C2/Nm2
-double CMf = (ep - em) / (ep + 2 * em);
+double CMfgeneral = (ep - em) / (ep + 2 * em);//a single CMf value is needed for dep energy profile, for monte carlo calc we use the specific CMf of each particle   
 double acceptedtrials = 0., totaltrials = 0., acceptancerate;
 int jmax = 200000000;
 double rad, A, B, MetropolisMC, kBoltzmann = 1.38064852e-23, Temp = 293.;
@@ -33,16 +33,20 @@ double number_of_slides_in_comsol_output = 30;
 double post_H = 4, pH = 9.43, R = Di * (1.0e-6) / 2., eps = 78.5, e0 = 1.60217657e-19, Na = 6.022e+23, zetasphere = -43.88, zetapost = -23.88;
 double A1w2, Asio2 = 6.6e-20, Aw = 3.7e-20;
 
-int const Particle_num = 2500;
+int const Particle_num = 1000;
 double paticles_cut_off_distance = 15.;
 
 string input = "30zSlices-albanie-2post-1MHz-z=30-v=5-normal-results-mesh0.6-0.8.txt"; //input file name
-string output = "out sio2 2500 particles di 20 dist 30 v5 1MHz.txt";//output file name
+string output = "out sio2 2500 particles di 20 dist 30 v5 1MHzmnmn.txt";//output file name
 bool plot_dep_enrgy = false;//which type output we want
 bool Xvalues = false, Yvalues = false, Zvalues = false, DEPvalues = true;
 double Zd_cross = 6;
-//const int numvalue = 565820;//as we know our data has this much row in it 565820
 
+string type1 = "sio2", type2 = "tio2";//particle types
+constexpr double portion_of_the_first_particle_type = 0.5;
+
+
+//const int numvalue = 565820;//as we know our data has this much row in it 565820
 
 struct triple {
 
@@ -111,7 +115,58 @@ double calcDistance(double x1, double x2, double y1, double y2, double z1, doubl
 		return sqrt(pow((x1 - x2), 2) + pow((ly - abs(y1 - y2)), 2) + pow((z1 - z2), 2));
 	}
 }
-double Upfde(double Es)
+
+class Particle
+{
+public:
+	double x[2];
+	double y[2];
+	double z[2];
+	double E[2];
+	double zeta;
+	double ep;
+	double Asphere;
+	double density;
+	double CMf;
+	string color;
+	void set_values(double xx, double yy, double zz, double EE, int i) {
+		x[i] = xx;
+		y[i] = yy;
+		z[i] = zz;
+		E[i] = EE;
+	}
+	void set_particle_properties(string type) {
+		if (type == "pmma") {//it means
+			color = "[1, 0.5, 0.5]";//red color 
+			zeta = -13.64;//mV
+			ep = 2.6 * eps0;
+			Asphere = 7.1e-20;//J
+			density = 1.19e6;//gr/m3
+			CMf = (ep - em) / (ep + 2 * em);
+		}
+		else if (type == "sio2") {
+			//color = "[224, 255, 255]";//kinda blue
+			color = "[0.6, 0.6, 1]";// light blue
+			zeta = -43.88;
+			ep = 3.8 * eps0;
+			Asphere = 6.6e-20;
+			density = 2.2e6;//gr/m3
+			CMf = (ep - em) / (ep + 2 * em);
+		}
+		else if (type == "tio2") {
+			color = "[0, 255, 0]";//green
+			zeta = -39.99;
+			ep = 80.* eps0;
+			Asphere = 43.e-20;
+			density = 3.9e6;
+			CMf = (ep - em) / (ep + 2 * em);
+		}
+	}
+
+
+};
+
+double Upfde(double Es, double CMf)
 {
 	double landa;// , EE0;
 	//EE0 = pow(8., -0.5)*Vpp / dg;
@@ -349,6 +404,9 @@ public:// for error
 
 
 
+
+
+
 class mutual_interactions {
 public:
 	double ys(double zeta)
@@ -495,29 +553,46 @@ int main() {
 	double Ed = 0.0;//Ed=Ederivd
 	Ed = energy_extraction.Energy_Value_Extaction(DataNum, DataSection, th, Xd, Yd, Zd, X, Y, Z, E, data_lx, data_ly, data_lz, d_lx);
 
-
-
-
 	int const Pnum = Particle_num;
+	constexpr double particle_ratio = portion_of_the_first_particle_type;
+
+	//creating paticle objects
+	constexpr int Pnumtype1 = Pnum * particle_ratio;// constexpr for const expressions 
+	//Create a course of students
+	//Particle Ps[Pnumtype1];//Ps: particles
+	Particle Ps[Pnum];//Ps: particles
+
+	//assigning poperties to two different prticle types making it suitable for binary assembly
+	for (i = 0; i < Pnum; i++) {
+		if (i < Pnumtype1) {
+			Ps[i].set_particle_properties(type1);
+		}
+		else {
+			Ps[i].set_particle_properties(type2);
+		}
+	}
+
+
 	int k = 0, ke = 0;
-	double Px[Pnum][2], Py[Pnum][2], Pz[Pnum][2], PEnergy[Pnum][2], A, B, A1, A2, A3, A4, A5, A6;//Di is in 1um
+	//double Px[Pnum][2], Py[Pnum][2], Pz[Pnum][2], PEnergy[Pnum][2], A, B, A1, A2, A3, A4, A5, A6;//Di is in 1um
+	double A, B, A1, A2, A3, A4, A5, A6;//Di is in 1um
+
 	int OverlapChance, ff = 0;
 	double G;
 	if (plot_dep_enrgy == false) {
 		for (i = 0; i < Pnum; i++) {
-			Px[i][0] = (double)fRand(0., d_lx);
-			Py[i][0] = (double)fRand(0., d_ly);
-
+			Ps[i].x[0] = (double)fRand(0., d_lx);
+			Ps[i].y[0] = (double)fRand(0., d_ly);
 			//debugged: in direction of Z box will start from 0 upto lz not from -lz/2 upto lz/2
 			//new debugged: based on exp(-kappa * (z - (Di / 2.))) z cannot be lower than radius of particle 'cause it will pass the beneath wall which is not real
-			Pz[i][0] = (double)fRand(z0 + 0.0001, d_lz);
+			Ps[i].z[0] = (double)fRand(z0 + 0.0001, d_lz);
 
 
 
 			for (k = (i - 1); k >= 0; k--) {
 
 				int OverlapChance;
-				A = calcDistance(Px[i][0], Px[k][0], Py[i][0], Py[k][0], Pz[i][0], Pz[k][0], d_lx, d_ly);
+				A = calcDistance(Ps[i].x[0], Ps[k].x[0], Ps[i].y[0], Ps[k].y[0], Ps[i].z[0], Ps[k].z[0], d_lx, d_ly);
 				//B = 1.05*Di; we make it dimensionless
 				B = 1.05;
 				if (A > B) {
@@ -532,19 +607,20 @@ int main() {
 					break;
 				}
 			}
+
 		}
 
 		for (k = 0; k < Pnum; k++) {
 			G = (double)zero;
 			//first we need to extract enrgy value of each point from our text data
-			Xd = Px[k][0], Yd = Py[k][0], Zd = Pz[k][0];
+			Xd = Ps[k].x[0], Yd = Ps[k].y[0], Zd = Ps[k].z[0];
 			Ed = energy_extraction.Energy_Value_Extaction(DataNum, DataSection, th, Xd, Yd, Zd, X, Y, Z, E, data_lx, data_ly, data_lz, d_lx);
 			//now we add Electrostatic among particles
 			for (ke = 0; ke < Pnum; ke++) {
 				if (ke == k) {
 					ke++;
 				}
-				A = calcDistance(Px[k][0], Px[ke][0], Py[k][0], Py[ke][0], Pz[k][0], Pz[ke][0], d_lx, d_ly);
+				A = calcDistance(Ps[k].x[0], Ps[ke].x[0], Ps[k].y[0], Ps[ke].y[0], Ps[k].z[0], Ps[ke].z[0], d_lx, d_ly);
 				B = paticles_cut_off_distance;
 				// new added: we need to add A > 340 because if there's a overlab the energy of system will increase to infinity						
 				if (A <= B) {
@@ -553,18 +629,18 @@ int main() {
 					G += mutual_interactions.EvdwSS(A) + mutual_interactions.ESt_SR_SR(A);
 				}
 			}
-			A1 = calcDistance(Px[k][0], (d_lx / 2.) - 0, Py[k][0], (d_ly / 2.) - 0, Pz[k][0], Pz[k][0], d_lx, d_ly);//we consider all z values equal as we only care about 2D distance of particles from the center of the posts  
-			A2 = calcDistance(Px[k][0], (d_lx / 2.) - bb, Py[k][0], (d_ly / 2.) - bb, Pz[k][0], Pz[k][0], d_lx, d_ly);
-			A3 = calcDistance(Px[k][0], (d_lx / 2.) - bb, Py[k][0], (d_ly / 2.) + bb, Pz[k][0], Pz[k][0], d_lx, d_ly);
-			A4 = calcDistance(Px[k][0], (d_lx / 2.) + bb, Py[k][0], (d_ly / 2.) - bb, Pz[k][0], Pz[k][0], d_lx, d_ly);
-			A5 = calcDistance(Px[k][0], (d_lx / 2.) + bb, Py[k][0], (d_ly / 2.) + bb, Pz[k][0], Pz[k][0], d_lx, d_ly);
-			A6 = calcDistance(Px[k][0], 0, Py[k][0], 0, Pz[k][0], Pz[k][0], d_lx, d_ly);
+			A1 = calcDistance(Ps[k].x[0], (d_lx / 2.) - 0, Ps[k].y[0], (d_ly / 2.) - 0, Ps[k].z[0], Ps[k].z[0], d_lx, d_ly);//we consider all z values equal as we only care about 2D distance of particles from the center of the posts  
+			A2 = calcDistance(Ps[k].x[0], (d_lx / 2.) - bb, Ps[k].y[0], (d_ly / 2.) - bb, Ps[k].z[0], Ps[k].z[0], d_lx, d_ly);
+			A3 = calcDistance(Ps[k].x[0], (d_lx / 2.) - bb, Ps[k].y[0], (d_ly / 2.) + bb, Ps[k].z[0], Ps[k].z[0], d_lx, d_ly);
+			A4 = calcDistance(Ps[k].x[0], (d_lx / 2.) + bb, Ps[k].y[0], (d_ly / 2.) - bb, Ps[k].z[0], Ps[k].z[0], d_lx, d_ly);
+			A5 = calcDistance(Ps[k].x[0], (d_lx / 2.) + bb, Ps[k].y[0], (d_ly / 2.) + bb, Ps[k].z[0], Ps[k].z[0], d_lx, d_ly);
+			A6 = calcDistance(Ps[k].x[0], 0, Ps[k].y[0], 0, Ps[k].z[0], Ps[k].z[0], d_lx, d_ly);
 
 			if ((A1 <= Diameter / 2.) || (A2 <= Diameter / 2.) || (A3 <= Diameter / 2.) || (A4 <= Diameter / 2.) || (A5 <= Diameter / 2.) || (A6 <= Diameter / 2.)) {
 				G += mutual_interactions.EDL_S_FP_bevan(Zd);
 			}
 
-			PEnergy[k][0] = Upfde(Ed) + G;
+			Ps[k].E[0] = Upfde(Ed, Ps[k].CMf) + G;
 		}
 
 		int j = 0;
@@ -580,63 +656,63 @@ int main() {
 			if (0. <= radnum && radnum < (1. / 3)) {
 
 				randdeltax = (double)fRand(-d_lx / 2.5, d_lx / 2.5);
-				Px[randi][1] = Px[randi][0] + randdeltax;
-				Py[randi][1] = Py[randi][0];
-				Pz[randi][1] = Pz[randi][0];
+				Ps[randi].x[1] = Ps[randi].x[0] + randdeltax;
+				Ps[randi].y[1] = Ps[randi].y[0];
+				Ps[randi].z[1] = Ps[randi].z[0];
 
 			}
 			else if ((1. / 3) <= radnum && radnum < (2. / 3)) {
 
 				randdeltay = (double)fRand(-d_ly / 2.5, d_ly / 2.5);
-				Px[randi][1] = Px[randi][0];
-				Py[randi][1] = Py[randi][0] + randdeltay;
-				Pz[randi][1] = Pz[randi][0];
+				Ps[randi].x[1] = Ps[randi].x[0];
+				Ps[randi].y[1] = Ps[randi].y[0] + randdeltay;
+				Ps[randi].z[1] = Ps[randi].z[0];
 
 			}
 			else {
 				randdeltaz = (double)fRand(-(d_lz - z0) / 5., (d_lz - z0) / 5.);
-				Px[randi][1] = Px[randi][0];
-				Py[randi][1] = Py[randi][0];
-				Pz[randi][1] = Pz[randi][0] + randdeltaz;
+				Ps[randi].x[1] = Ps[randi].x[0];
+				Ps[randi].y[1] = Ps[randi].y[0];
+				Ps[randi].z[1] = Ps[randi].z[0] + randdeltaz;
 			}
 
 			//pbc
 			//pbc for x direction
-			if (Px[randi][1] > d_lx) {
+			if (Ps[randi].x[1] > d_lx) {
 
-				Px[randi][1] = Px[randi][1] - d_lx;
+				Ps[randi].x[1] = Ps[randi].x[1] - d_lx;
 			}
-			else if (Px[randi][1] < 0.) {
+			else if (Ps[randi].x[1] < 0.) {
 
-				Px[randi][1] = Px[randi][1] + d_lx;
+				Ps[randi].x[1] = Ps[randi].x[1] + d_lx;
 			}
 			//pbc for y direction
-			if (Py[randi][1] > d_ly) {
+			if (Ps[randi].y[1] > d_ly) {
 
-				Py[randi][1] = Py[randi][1] - d_ly;
+				Ps[randi].y[1] = Ps[randi].y[1] - d_ly;
 			}
-			else if (Py[randi][1] < 0.) {
+			else if (Ps[randi].y[1] < 0.) {
 
-				Py[randi][1] = Py[randi][1] + d_ly;
+				Ps[randi].y[1] = Ps[randi].y[1] + d_ly;
 			}
 
 			//pbc for z direction
-			if (Pz[randi][1] > d_lz) {
+			if (Ps[randi].z[1] > d_lz) {
 
 				//Pz[randi][1] = lz;
-				Pz[randi][1] = Pz[randi][1] - (d_lz - z0);
+				Ps[randi].z[1] = Ps[randi].z[1] - (d_lz - z0);
 			}
-			else if (Pz[randi][1] < z0) {
+			else if (Ps[randi].z[1] < z0) {
 				//this should not happen as we have always reulsion with the below surface 
 				//Pz[randi][1] = z0+0.001;
-				Pz[randi][1] = Pz[randi][1] + (d_lz - z0);
+				Ps[randi].z[1] = Ps[randi].z[1] + (d_lz - z0);
 
 			}
 			o = 0;
 			for (i = 0; i < Pnum; i++) {
 				ff = 0;
 				if (i != randi) {
-					A = calcDistance(Px[randi][1], Px[i][0], Py[randi][1], Py[i][0], Pz[randi][1], Pz[i][0], d_lx, d_ly);
+					A = calcDistance(Ps[randi].x[1], Ps[i].x[0], Ps[randi].y[1], Ps[i].y[0], Ps[randi].z[1], Ps[i].z[0], d_lx, d_ly);
 					//B = 1.05*Di; we make it dimensionless
 					B = 1.005;
 					if (A > B) {
@@ -656,12 +732,12 @@ int main() {
 					if (o == (Pnum - 1)) {
 						G = (double)zero;
 						//Px[randi][1]= (d_lx / 1.)-3, Py[randi][1]= (d_ly / 1.)-3, Pz[randi][1]=4.01;
-						Xd = Px[randi][1], Yd = Py[randi][1], Zd = Pz[randi][1];
+						Xd = Ps[randi].x[1], Yd = Ps[randi].y[1], Zd = Ps[randi].z[1];
 						//now a loop to find PEnergy value by searching through the Edelta2 values 
 						Ed = energy_extraction.Energy_Value_Extaction(DataNum, DataSection, th, Xd, Yd, Zd, X, Y, Z, E, data_lx, data_ly, data_lz, d_lx);
 						for (ke = 0; ke < Pnum; ke++) {
 							if (ke != randi) {
-								A = calcDistance(Px[randi][1], Px[ke][1], Py[randi][1], Py[ke][1], Pz[randi][1], Pz[ke][1], d_lx, d_ly);
+								A = calcDistance(Ps[randi].x[1], Ps[ke].x[1], Ps[randi].y[1], Ps[ke].y[1], Ps[randi].z[1], Ps[ke].z[1], d_lx, d_ly);
 								B = paticles_cut_off_distance;
 								// new added: we need to add A > 340 because if there's a overlab the energy of system will increase to infinity						
 								if (A <= B) {
@@ -672,12 +748,12 @@ int main() {
 								}
 							}
 						}
-						A1 = calcDistance(Px[randi][1], (d_lx / 2.) - 0, Py[randi][1], (d_ly / 2.) - 0, Pz[randi][1], Pz[randi][1], d_lx, d_ly);
-						A2 = calcDistance(Px[randi][1], (d_lx / 2.) - bb, Py[randi][1], (d_ly / 2.) - bb, Pz[randi][1], Pz[randi][1], d_lx, d_ly);
-						A3 = calcDistance(Px[randi][1], (d_lx / 2.) - bb, Py[randi][1], (d_ly / 2.) + bb, Pz[randi][1], Pz[randi][1], d_lx, d_ly);
-						A4 = calcDistance(Px[randi][1], (d_lx / 2.) + bb, Py[randi][1], (d_ly / 2.) - bb, Pz[randi][1], Pz[randi][1], d_lx, d_ly);
-						A5 = calcDistance(Px[randi][1], (d_lx / 2.) + bb, Py[randi][1], (d_ly / 2.) + bb, Pz[randi][1], Pz[randi][1], d_lx, d_ly);
-						A6 = calcDistance(Px[randi][1], 0, Py[randi][1], 0, Pz[randi][1], Pz[randi][1], d_lx, d_ly);
+						A1 = calcDistance(Ps[randi].x[1], (d_lx / 2.) - 0, Ps[randi].y[1], (d_ly / 2.) - 0, Ps[randi].z[1], Ps[randi].z[1], d_lx, d_ly);
+						A2 = calcDistance(Ps[randi].x[1], (d_lx / 2.) - bb, Ps[randi].y[1], (d_ly / 2.) - bb, Ps[randi].z[1], Ps[randi].z[1], d_lx, d_ly);
+						A3 = calcDistance(Ps[randi].x[1], (d_lx / 2.) - bb, Ps[randi].y[1], (d_ly / 2.) + bb, Ps[randi].z[1], Ps[randi].z[1], d_lx, d_ly);
+						A4 = calcDistance(Ps[randi].x[1], (d_lx / 2.) + bb, Ps[randi].y[1], (d_ly / 2.) - bb, Ps[randi].z[1], Ps[randi].z[1], d_lx, d_ly);
+						A5 = calcDistance(Ps[randi].x[1], (d_lx / 2.) + bb, Ps[randi].y[1], (d_ly / 2.) + bb, Ps[randi].z[1], Ps[randi].z[1], d_lx, d_ly);
+						A6 = calcDistance(Ps[randi].x[1], 0, Ps[randi].y[1], 0, Ps[randi].z[1], Ps[randi].z[1], d_lx, d_ly);
 
 						if ((A1 <= Diameter / 2.) || (A2 <= Diameter / 2.) || (A3 <= Diameter / 2.) || (A4 <= Diameter / 2.) || (A5 <= Diameter / 2.) || (A6 <= Diameter / 2.)) {
 							//double Gu= mutual_interactions.SEIntegralSumEq(0, R, 500, Zd);
@@ -685,15 +761,15 @@ int main() {
 							G += mutual_interactions.EDL_S_FP_bevan(Zd);
 						}
 
-						PEnergy[randi][1] = Upfde(Ed) + G;
+						Ps[randi].E[1] = Upfde(Ed, Ps[randi].CMf) + G;
 
-						if (PEnergy[randi][0] >= PEnergy[randi][1]) {
+						if (Ps[randi].E[0] >= Ps[randi].E[1]) {
 							// false == 0 and true = !false
 							MetropolisMC = true;
 						}
 
 						else {
-							double P = exp(-(PEnergy[randi][1] - PEnergy[randi][0]) / (kBoltzmann*Temp));
+							double P = exp(-(Ps[randi].E[1] - Ps[randi].E[0]) / (kBoltzmann*Temp));
 							//double P = exp(-(TotalEnergy[1] - TotalEnergy[0]) / (kBoltzmann*Temp));
 							rad = (double)fRand(0., 1.);
 							//Yes, the random number r should be less than or equal to p = exp(-Delta E/kT). This is right.
@@ -719,10 +795,11 @@ int main() {
 							//	cout << "";
 							//}
 
-							Px[randi][0] = Px[randi][1];
-							Py[randi][0] = Py[randi][1];
-							Pz[randi][0] = Pz[randi][1];
-							PEnergy[randi][0] = PEnergy[randi][1];
+							Ps[randi].x[0] = Ps[randi].x[1];
+							Ps[randi].y[0] = Ps[randi].y[1];
+							Ps[randi].z[0] = Ps[randi].z[1];
+							Ps[randi].E[0] = Ps[randi].E[1];
+
 
 
 
@@ -731,12 +808,12 @@ int main() {
 							//cout << "\n\n " << "acceptance rate: " << acceptancerate << "\n\n";
 							int pr;
 							//to get monte carlo steps per each sphere
-							pr = fmod(j, (Pnum));
+							pr = fmod(j, (1));
 							if (pr == 0) {
 								cout << "j=" << j << "\n\n";
 								for (i = 0; i < Pnum; i++) {
 									//each particle has a specific color 
-									cout << "{RGBColor[224, 255, 255], Opacity[0.8], Sphere[{" << Px[i][0] << ", " << Py[i][0] << ", " << Pz[i][0] << "}, " << (Di / 2.) << "]},";
+									cout << "{RGBColor" << Ps[i].color << ", Opacity[0.8], Sphere[{" << Ps[i].x[0] << ", " << Ps[i].y[0] << ", " << Ps[i].z[0] << "}, " << (Di / 2.) << "]},";
 								}
 							}
 							//if (pr == 0) {
@@ -803,7 +880,7 @@ int main() {
 				for (j = 0; (j*deltay) <= d_ly; j++) {
 					Xd = i * deltax, Yd = j * deltay;
 					Ed = energy_extraction.Energy_Value_Extaction(DataNum, DataSection, th, Xd, Yd, Zd, X, Y, Z, E, data_lx, data_ly, data_lz, d_lx);
-					cout << Upfde(Ed) << "\n"; ;
+					cout << Upfde(Ed, CMfgeneral) << "\n"; ;
 				}
 			}
 		}
